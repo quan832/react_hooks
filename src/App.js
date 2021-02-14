aec53ac3d16dccc6bd8409a9d7aa046d5788769d
quantran2381@gmail.com <quantran2381@gmail.com>
2021-02-14 11:17:58 +0700

    todo-list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import { useState } from 'react';
 import './App.scss';
 import ColorBox from './Components/colorBox';
+import TodoList from './Components/TodoList';
 
 function App() {
+  const [todoList, setTodoList] = useState(
+    [
+      { id: 1, title: 'I love Easy Frontend! 😍 ' },
+      { id: 2, title: 'We love Easy Frontend! 🥰 ' },
+      { id: 3, title: 'They love Easy Frontend! 🚀 ' },
+    ]
+  );
   return (
     <div className="App">
       <h1>React-Hooks-Basic</h1>
-      <ColorBox></ColorBox>
+      {/* <ColorBox></ColorBox> */}
+      <TodoList todos={todoList}></TodoList>
     </div>
   );
 }
---
+++ src/Components/TodoList/index.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+TodoList.propTypes = {
+  todos: PropTypes.array,
+  onTodoClick: PropTypes.func,
+};
+
+TodoList.defaultProps = {
+  todos: [],
+  onTodoClick: null,
+};
+function TodoList(props) {
+  const { todos, onTodoClick } = props;
+
+  return (
+    <div>
+      <ul className="todo-list">
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default TodoList;
